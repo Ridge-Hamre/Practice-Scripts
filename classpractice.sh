1d0281dc961404e18aeb86cba9dc424c1b98c54f
ridge731 <ridge731@gmail.com>
2017-07-13 13:22:31 -0400

    added file classpractice.sh

---
+++ classpractice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Current directory: `pwd`"
+echo ""
+echo "Current directory files are:"
+echo ""
+ls -lah
+echo ""
+echo "Today's date is: `date`"
+echo ""
+echo "The system name is `whoami` on `hostname` (`hostname -i`)"
